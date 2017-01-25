fc396ce4cd062915c59d827ac8f3c645da919c11
Baptiste Veyssiere <baptiste.veyssiere@epitech.eu>
2017-01-25 10:57:45 +0100

    first_fit init

---
+++ free.c
@@ -0,0 +1,9 @@
+/*
+** free.c for first fit in /home/veyssi_b/rendu/tek2/PSU/PSU_2016_malloc
+** 
+** Made by Baptiste Veyssiere
+** Login   <veyssi_b@epitech.net>
+** 
+** Started on  Wed Jan 25 10:57:32 2017 Baptiste Veyssiere
+** Last update Wed Jan 25 10:57:32 2017 Baptiste Veyssiere
+*/
