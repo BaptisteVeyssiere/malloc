bc0bad79c0a36b59577aa192eee06d4b7431f98e
Nathan Scutari <nathan.scutari@epitech.eu>
2017-01-31 21:43:46 +0100

    boucle inf

--- src/main_test.c
+++ src/main_test.c
@@ -5,7 +5,7 @@
 ** Login   <scutar_n@epitech.net>
 **
 ** Started on  Tue Jan 31 10:22:04 2017 Nathan Scutari
-** Last update Tue Jan 31 16:09:21 2017 Nathan Scutari
+** Last update Tue Jan 31 21:39:38 2017 Nathan Scutari
 */
 
 #include "malloc.h"
@@ -13,13 +13,36 @@
 int	main()
 {
   char		*str;
+  char		*str2;
+  char		*str3;
+  char		*str4;
 
-  malloc(10);
-  malloc(500);
-  malloc(9753);
-  free(malloc(1230));
-  malloc(2);
-  malloc(9);
-  malloc(1000);
+  str = malloc(552);
+  str2 = malloc(120);
+  str3 = malloc(1024);
+  free(str2);
+  free(str3);
+  free(str);
+  str = malloc(5);
+  free(str);
+  str = malloc(552);
+  str2 = malloc(4096);
+  str3 = malloc(1600);
+  str4 = malloc(1024);
+  free(str4);
+  str4 = malloc(2048);
+  free(str2);
+  free(str);
+  str = malloc(5);
+  free(str);
+  str = malloc(45);
+  free(str);
+  malloc(45);
+  malloc(72);
+  malloc(40);
+  malloc(56);
+  malloc(37);
+  malloc(48);
+  malloc(42);
   return (0);
 }
